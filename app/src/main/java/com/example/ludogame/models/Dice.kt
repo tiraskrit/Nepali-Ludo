9c871403a6df94a22c65b513858c5b2f010c33c0
Uttam Dhakal <e_uttam@yahoo.com>
2024-07-26 11:44:17 +0200

    random dice

--- app/src/main/java/com/example/ludogame/models/Dice.kt
+++ app/src/main/java/com/example/ludogame/models/Dice.kt
@@ -1,6 +1,5 @@
 package com.example.ludogame.models
 
 class Dice {
-    fun roll(): Int = (1..1).random() // for test
-    //fun roll(): Int = (1..6).random()
+    fun roll(): Int = (1..6).random()
 }
